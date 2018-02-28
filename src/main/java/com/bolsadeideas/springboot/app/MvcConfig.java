911e92ede27f190c90bb06f79d9e1c6e45a05d42
luisandia <luisandiamamani@gmail.com>
2018-02-28 17:41:15 +0000

    upload image by method verFoto in controller

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.nio.file.Paths;
 
-@Configuration
+/*@Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -24,3 +24,4 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
 	
 }
+*/
